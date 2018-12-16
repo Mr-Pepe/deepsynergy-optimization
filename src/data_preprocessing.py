d4e4e2291e86e7f59e61fec9ffb13b582d415f03
Felipe <felipepeter@web.de>
2018-12-16 22:04:08 +0800

    Implemented normalization + tanh

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -2,10 +2,6 @@ import pickle
 import gzip
 import torch
 import pandas as pd
-import matplotlib.pyplot as plt
-from src.model import SynergyNetwork
-
-n_datapoints = 1000
 
 
 ## Load the input data
@@ -14,11 +10,19 @@ loadFile = gzip.open("../datasets/X.p.gz")
 X = pickle.load(loadFile)
 loadFile.close()
 
-X = X[:n_datapoints]
 X = torch.from_numpy(X)
 X = X.float()
 
-saveFile = open("../datasets/x.p", 'wb')
+# Normalize + tanh
+means       = X.mean(dim=0)
+std_devs    = X.std(dim=0)
+mask = std_devs != 0
+
+X = X-means
+X[:,mask] = X[:,mask]/std_devs[mask]
+X.tanh_()
+
+saveFile = open("../datasets/x_norm_tanh.p", 'wb', pickle.HIGHEST_PROTOCOL)
 pickle.dump(X, saveFile)
 saveFile.close()
 
@@ -27,10 +31,9 @@ saveFile.close()
 labels = pd.read_csv("../datasets/labels.csv", index_col=0)
 
 synergy_scores = labels.values[:,3].astype('float')
-synergy_scores = synergy_scores[:n_datapoints]
-# synergy_scores = synergy_scores.to
 synergy_scores = torch.from_numpy(synergy_scores)
 synergy_scores = synergy_scores.float()
+synergy_scores = torch.cat([synergy_scores, synergy_scores], dim=0)
 
 saveFile = open("../datasets/synergy_scores.p", 'wb')
 pickle.dump(synergy_scores, saveFile)
