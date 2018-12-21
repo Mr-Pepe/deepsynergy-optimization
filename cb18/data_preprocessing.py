00396b605f0ef6ebe95594c592f677cd1c18692c
Felipe <felipepeter@web.de>
2018-12-21 13:23:08 +0800

    Added with-statements

--- cb18/data_preprocessing.py
+++ cb18/data_preprocessing.py
@@ -6,14 +6,12 @@ import numpy as np
 
 
 ## Load the input data
-print("Loading input features")
-loadFile = gzip.open("../datasets/X.p.gz")
-
-X = pickle.load(loadFile)
-loadFile.close()
-
-X = torch.from_numpy(X)
-X = X.float()
+print("Loading input features ... ", end='')
+with gzip.open("../datasets/X.p.gz") as loadFile:
+    X = pickle.load(loadFile)
+    X = torch.from_numpy(X)
+    X = X.float()
+print("Done.")
 
 ## Load the output data
 print("Loading labels ... ", end='')
@@ -25,7 +23,7 @@ print("Split data ... ", end='')
 synergy_scores = labels.values[:,3].astype('float')
 synergy_scores = torch.from_numpy(synergy_scores)
 synergy_scores = synergy_scores.float()
-print("Done!")
+
 
 idx = labels.values[:,4].astype('int')
 train_idx   = np.where(idx != 0)
@@ -36,15 +34,15 @@ X_test  = X[test_idx]
 
 y_train = synergy_scores[train_idx]
 y_test  = synergy_scores[test_idx]
-
+print("Done!")
 
 # Save
-print("Saving training dataset")
-saveFile = open("../datasets/train_data.p", 'wb')
-pickle.dump((X_train, y_train), saveFile)
-saveFile.close()
+print("Saving training dataset ... ", end='')
+with open("../datasets/train_data.p", 'wb') as saveFile:
+    pickle.dump((X_train, y_train), saveFile)
+print("Done.")
 
-print("Saving test dataset")
-saveFile = open("../datasets/test_data.p", 'wb')
-pickle.dump((X_test, y_test), saveFile)
-saveFile.close()
+print("Saving test dataset ... ", end='')
+with open("../datasets/test_data.p", 'wb') as saveFile:
+    pickle.dump((X_test, y_test), saveFile)
+print("Done.")
