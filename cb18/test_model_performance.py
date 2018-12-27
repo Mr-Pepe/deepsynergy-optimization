1300d819645e6ce4d410927e44c005b122d315eb
Felipe <felipepeter@web.de>
2018-12-27 14:45:40 +0800

    Extended model performance test to use mean of multiple models

--- cb18/test_model_performance.py
+++ cb18/test_model_performance.py
@@ -6,18 +6,19 @@ import os
 import os.path
 
 test_data_path  = "../datasets/test_data.p"
-model_path      = None
+model_paths      = ['/home/felipe/Projects/cb18/saves/search20181224210125/train20181225205131/model807',
+                    '/home/felipe/Projects/cb18/saves/search20181224210125/train20181225214100/model518',
+                    '/home/felipe/Projects/cb18/saves/search20181224210125/train20181226104223/model697']
 model_dir_path  = '/home/felipe/Projects/cb18/saves/gridSearch'
 
+# mode 1: evaluate models on their own
+# mode 2: evaluate model mean
+mode = 2
 
-
-if model_path is None:
-    model_paths = []
+if model_paths is None:
     for dirpath, dirnames, filenames in os.walk(model_dir_path):
         for filename in [f for f in filenames if f.find('model') != -1]:
             model_paths.append(os.path.join(dirpath, filename))
-else:
-    model_paths = [model_path]
 
 # Load test dataset
 print("Loading test dataset ... ", end='')
@@ -28,30 +29,77 @@ test_data_file.close()
 dataset = utils.Dataset(X, y)
 print("Done.")
 
-best_mse = 0
+if mode == 1:
 
-for path in model_paths:
+    best_mse = 0
 
-    print("Loading model " + path + " ... ", end='')
-    model = torch.load(path)
-    model.eval()
-    print("Done.")
+    for path in model_paths:
 
-    print("Evaluating model ... ", end='')
-    pred_scores = model(X)
-    print("Done.")
+        print("Loading model " + path + " ... ", end='')
+        model = torch.load(path)
+        model.eval()
+        print("Done.")
 
-    mse = pred_scores[:,0]-y
-    mse = mse.pow(2)
-    mse = mse.sum()/len(mse)
+        print("Evaluating model ... ", end='')
+        pred_scores = model(X)
+        print("Done.")
 
-
-    print("Test score: " + str(mse.item()))
-
-    if best_mse == 0 or mse < best_mse:
-        best_mse = mse
-        best_model_path = path
-        print("New best MSE.")
+        mse = pred_scores[:,0]-y
+        mse = mse.pow(2)
+        mse = mse.sum()/len(mse)
 
 
-print("Best model: " + best_model_path)
+        print("Test score: " + str(mse.item()))
+
+        if best_mse == 0 or mse < best_mse:
+            best_mse = mse
+            best_model_path = path
+            print("New best MSE.")
+
+
+    print("Best model: " + best_model_path)
+
+elif mode == 2:
+
+    overall_pred_scores = None
+    n_overall_pred_scores = 0
+    overall_mse = 0
+    mse_combined_history = []
+    mse_history = []
+
+    for path in model_paths:
+
+        print("Loading model " + path + " ... ", end='')
+        model = torch.load(path)
+        model.eval()
+        print("Done.")
+
+        print("Evaluating model ... ", end='')
+        pred_scores = model(X)
+        del model
+        pred_scores = pred_scores.detach()
+        print("Done.")
+
+        if overall_pred_scores is None:
+            overall_pred_scores = torch.t(pred_scores)
+            n_overall_pred_scores = 1
+        else:
+            overall_pred_scores = n_overall_pred_scores / (n_overall_pred_scores + 1) * overall_pred_scores + torch.t(
+                pred_scores) / (n_overall_pred_scores + 1)
+            n_overall_pred_scores += 1
+            # overall_pred_scores = torch.cat((overall_pred_scores, torch.t(pred_scores)), 0)
+
+        mse = pred_scores[:, 0] - y
+        mse = mse.pow(2)
+        mse = mse.sum() / len(mse)
+
+        mse_history.append(mse.item())
+
+        mse_combined = overall_pred_scores.mean(dim=0) - y
+        mse_combined = mse_combined.pow(2)
+        mse_combined = mse_combined.sum() / len(mse_combined)
+
+        mse_combined_history.append(mse_combined.item())
+
+        print("Val score this model: " + "{0:.3f}".format(mse.item()) + "  Combined: " + "{0:.3f}".format(
+            mse_combined.item()))
