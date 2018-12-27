2f2793f8894ead1df6e27f5c591c2cb19963f6a2
Felipe <felipepeter@web.de>
2018-12-27 15:13:48 +0800

    Added learning rate decay to hyperparameter optimization

--- cb18/train_model.py
+++ cb18/train_model.py
@@ -80,11 +80,13 @@ utility = UtilityFunction(kind="ucb", kappa=2.5, xi=0)
 
 optimizer = BayesianOptimization(
     f='',
-    pbounds={'batch_size':   (0, 1),
-            'n_hidden_1':   (32, 4096),
-            'n_hidden_2':   (32, 4096),
-            'learning_rate': (1e-5, 1e-3),
-            'dropout':      (0, 1)},
+    pbounds={'batch_size':   (0, 0.4),
+            'n_hidden_1':   (32, 1600),
+            'n_hidden_2':   (2000, 4000),
+            'learning_rate': (1e-6, 1e-4),
+            'dropout':      (0.05, 0.35),
+             'lr_decay':    (0.5, 0.9),
+             'lr_decay_interval': (10, 100)},
     verbose=0,
     random_state=5
 )
@@ -111,8 +113,8 @@ for i in range(config['n_bayes_steps']):
     learning_rate = float(next['learning_rate'])
     dropout = float(next['dropout'])
     batch_norm = config['batch_norm']
-    lr_decay = config['lr_decay']
-    lr_decay_interval = config['lr_decay_interval']
+    lr_decay = float(next['lr_decay'])
+    lr_decay_interval = int(next['lr_decay_interval'])
 
     train_data_loader = torch.utils.data.DataLoader(dataset=train_set,
                                                     batch_size=batch_size,
