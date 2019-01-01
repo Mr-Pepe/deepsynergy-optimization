fbc74fac40dc8601fb3c5880927c6f32504b26af
Felipe <felipepeter@web.de>
2019-01-01 21:35:31 +0800

    Do SVD on test folds

--- cb18/svd.py
+++ cb18/svd.py
@@ -5,38 +5,53 @@ import numpy as np
 import torch
 import cb18.utils as utils
 
-train_data_path     = "../datasets/train_data.p"
-fold_indices_path   = "../datasets/fold_indices.p"
-svd_save_paths      = ["../datasets/svd%d.p" % i for i in range(4)]
 
-print("Loading train dataset ... ", end='')
-with open(train_data_path, 'rb') as file:
-    X, y = pickle.load(file)
-print("Done.")
+num_train_folds = 5
+num_eigenvectors = 80
 
-print("Loading fold indices ...", end='')
-with open(fold_indices_path, 'rb') as file:
-    fold_indices = pickle.load(file)
-print("Done.")
+train_data_path     = ["../datasets/train_data_fold_%d.p" % i for i in range(5)]
+svd_save_paths      = [["../datasets/svd_test_fold_%d_train_fold_%d.p" % (i_test_fold, i_train_fold) for i_train_fold in range(num_train_folds)] for i_test_fold in range(5)]
 
-# Calculate the SVD on the train data sets according to the folding indices
-for i in range(4):
-    train_indices = fold_indices[i][0]
+for i_test_fold in range(5):
 
-    # Use tensor cloning to copy the data from the original tensor
-    # It otherwise changes the original data during normalization
-    X_train = X[train_indices].clone().detach()
-
-    print("Normalize train data of fold %d ... " % i)
-    X_train, means, std_devs = utils.normalize(X_train, tanh=False)
-
-    print("Doing SVD decomposition for fold %d ... " % i, end='')
-    U, S, V = torch.svd(X_train)
+    print("Loading train dataset for test fold %d... " % i_test_fold, end='')
+    with open(train_data_path[i_test_fold], 'rb') as file:
+        X, y = pickle.load(file)
     print("Done.")
 
-    del X_train
+    np.random.seed(0)
+    indices = np.arange(len(X))
+    np.random.shuffle(indices)
+    fold_indices = np.array_split(indices, num_train_folds)
 
-    print("Saving U, S, V for fold %d ... " % i, end='')
-    with open(svd_save_paths[i], 'wb') as file:
-        pickle.dump((U, S, V), file)
-    print("Done.")
+    # Calculate the SVD on the train data sets according to the folding indices
+    for i_train_fold in range(num_train_folds):
+
+        train_indices = np.delete(indices, np.where(np.isin(indices, fold_indices[i_train_fold])))
+
+
+
+        # Use tensor cloning to copy the data from the original tensor
+        # It otherwise changes the original data during normalization
+        X_train = X[train_indices].clone().detach()
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        print("Using device: " + device.type)
+
+        X_train.to(device)
+
+        print("Normalize train data of test fold %d train fold %d ... " % (i_test_fold, i_train_fold))
+        X_train, means, std_devs = utils.normalize(X_train, tanh=False)
+
+        print("Doing SVD decomposition for test fold %d train fold %d ... " % (i_test_fold, i_train_fold), end='')
+        U, S, V = X_train.svd()
+        print("Done.")
+
+        del X_train
+
+        # Only save the num_eigenvectors most important vectors of V for memory saving
+        print("Saving V for test fold %d train fold %d ... " % (i_test_fold, i_train_fold), end='')
+        with open(svd_save_paths[i_test_fold][i_train_fold], 'wb') as file:
+            pickle.dump(V[:, :num_eigenvectors], file)
+        print("Done.")
--- cb18/svd_reconstruction.py
+++ cb18/svd_reconstruction.py
@@ -4,46 +4,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 
-train_data_path   = "../datasets/train_data.p"
-fold_indices_path = "../datasets/fold_indices.p"
-svd_load_paths    = ["../datasets/svd%d.p" % i for i in range(4)]
+num_train_folds = 5
+num_eigenvectors = 80
 
-print("Loading train dataset ... ", end='')
-with open(train_data_path, 'rb') as train_data_file:
-    X, y = pickle.load(train_data_file)
-print("Done.")
-
-print("Loading fold indices ...", end='')
-with open(fold_indices_path, 'rb') as file:
-    fold_indices = pickle.load(file)
-print("Done.")
+train_data_path     = ["../datasets/train_data_fold_%d.p" % i for i in range(5)]
+svd_load_paths      = [["../datasets/svd_test_fold_%d_train_fold_%d.p" % (i_test_fold, i_train_fold) for i_train_fold in range(num_train_folds)] for i_test_fold in range(5)]
 
 
-for i in range(4):
+for i_test_fold in range(5):
 
-    train_indices = fold_indices[i][0]
-
-    print("Loading U, S, V for fold %d ... " % i, end='')
-    with open(svd_load_paths[i], 'rb') as file:
-        U, S, V = pickle.load(file)
+    print("Loading train dataset for test fold %d... " % i_test_fold, end='')
+    with open(train_data_path[i_test_fold], 'rb') as file:
+        X, y = pickle.load(file)
     print("Done.")
 
-    # Use tensor cloning to copy the data from the original tensor
-    # It otherwise changes the original data during normalization
-    X_train = X[train_indices].clone().detach()
+    np.random.seed(0)
+    indices = np.arange(len(X))
+    np.random.shuffle(indices)
+    fold_indices = np.array_split(indices, num_train_folds)
 
-    print("Normalize train data of fold %d ... " % i)
-    X_train, means, std_devs = utils.normalize(X_train, tanh=False)
+    for i_train_fold in range(num_train_folds):
 
-    var = torch.cumsum(S.pow(2) / S.pow(2).sum(), 0)
+        train_indices = np.delete(indices, np.where(np.isin(indices, fold_indices[i_train_fold])))
 
-    for k in [1, 10, 50, 80, 106, 107, 108, 200, 500]:
-        print("Reconstructing with k=" + str(k), " ... ", end='')
-        X_reconstructed = torch.matmul(torch.matmul(U[:, :k], torch.diag(S[:k])), torch.t(V[:, :k]))
+        print("Loading V for test fold %d train fold %d ... " % (i_test_fold, i_train_fold), end='')
+        with open(svd_load_paths[i_test_fold][i_train_fold], 'rb') as file:
+            V = pickle.load(file)
+        print("Done.")
+
+        # Use tensor cloning to copy the data from the original tensor
+        # It otherwise changes the original data during normalization
+        X_train = X[train_indices].clone().detach()
+
+        print("Normalize train data of test fold %d train fold %d ... " % (i_test_fold, i_train_fold))
+        X_train, means, std_devs = utils.normalize(X_train, tanh=False)
+
+        print("Reconstructing ...", end='')
+        X_reconstructed = torch.matmul(X_train, V)
 
         print("MSE: " + str(utils.mse(X_train, X_reconstructed)))
 
-    plt.plot(var[:200].numpy())
-    plt.xlabel("Ordered Eigenvectors")
-    plt.ylabel("Covered Variance")
-    plt.show()
